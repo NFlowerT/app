6e1df696fa56ab5203ac260526064953e169c13e
Maksiu 3 <maks.ga@wp.pl>
2022-04-16 22:41:10 +0200

    Nav fixed Hero

--- components/global/navbar.jsx
+++ components/global/navbar.jsx
@@ -1,52 +1,52 @@
-import React from 'react';
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { BiUserCircle } from 'react-icons/bi'
-import Image from 'next/image'
+import React from "react"
+import Link from "next/link"
+import { useRouter } from "next/router"
+import { BiUserCircle } from "react-icons/bi"
+import Image from "next/image"
 import style from "../../styles/global/nav.module.scss"
-import Wallet from "../wallet/wallet";
+import Wallet from "../wallet/wallet"
 
 const Navbar = ({setAccount, loadBlockChainData}) => {
-    const router = useRouter()
-    const location = router.pathname
-    console.log(location)
+	const router = useRouter()
+	const location = router.pathname
+	console.log(location)
 
-    return (
-        <nav className={style.nav}>
-            <Link className={style.logoContainer} href={'/'}>
-                <div>
-                    <div className={'logoIcon'}>
-                    </div>
-                    <div className={style.logoTitle }><h4>FORESTA</h4></div>
-                </div>
+	return (
+		<nav className={style.nav}>
 
-            </Link>
-            <Wallet setAccount={setAccount} loadBlockChainData={async () =>await loadBlockChainData()}></Wallet>
-            <div className={style.categoryContainer}>
-                <div>
-                    <Link className={'router ' + (location === '/' ? 'selected' : ' ')} href={'/'}><h4>HOME</h4></Link>
-                </div>
-                <div>
-                    <Link className={'router ' + (location === '/gallery' ? 'selected' : ' ')} href={'/gallery'}><h4>GALLERY</h4></Link>
-                </div>
-                <div>
-                    <Link className={'router ' + (location === '/market' ? 'selected' : ' ')} href={'/market'}><h4>MARKET</h4></Link>
-                </div>
-                <div className={style.accountIcon}>
-                    <Link className={style.routerAccount} href={'/user'}>
-                        <BiUserCircle></BiUserCircle>
-                    </Link>
-                </div>
-            </div>
+			<div className={style.logoContainer}>
+				<Link href={"/"}>
+					<div className={style.logoTitle }>FORESTA</div>
+				</Link>
+			</div>
 
-            {/*<div>*/}
-            {/*    <Link className={style.routerAccount} href={'/'}>*/}
-            {/*        <BiUserCircle></BiUserCircle>*/}
-            {/*    </Link>*/}
-            {/*</div>*/}
+			<Wallet setAccount={setAccount} loadBlockChainData={async () =>await loadBlockChainData()}></Wallet>
 
-        </nav>
-    );
-};
+			<div className={style.categoryContainer}>
+				<div>
+					<Link href={"/"}><div>SAPPLINGS</div></Link>
+				</div>
+				<div>
+					<Link href={"/gallery"}><div>GALLERY</div></Link>
+				</div>
+				<div>
+					<Link href={"/market"}><div>MARKET</div></Link>
+				</div>
+				<div  className={style.AccountIcon}>
+					<Link href={"/user"}>
+						<BiUserCircle></BiUserCircle>
+					</Link>
+				</div>
+			</div>
 
-export default Navbar;
+			{/*<div>*/}
+			{/*    <Link className={style.routerAccount} href={'/'}>*/}
+			{/*        <BiUserCircle></BiUserCircle>*/}
+			{/*    </Link>*/}
+			{/*</div>*/}
+
+		</nav>
+	)
+}
+
+export default Navbar
--- components/home/hero.jsx
+++ components/home/hero.jsx
@@ -3,9 +3,17 @@ import style from "../../styles/home/hero.module.scss"
 
 const Hero = ({mint}) => {
 	return (
-		<div className={style.container}>
-			Hero section
-			<button onClick={async ()=>await mint()}> buy tree</button>
+		<div className={style.heroMain}>
+			<div className={style.heroLogoContainer}>
+				<div className={style.heroLogo}>
+					<h1>FORESTA</h1>
+					<p>MAKE YOUR FOREST FLY</p>
+				</div>
+
+			</div>
+			<div className={style.heroTreeContainer}>
+
+			</div>
 		</div>
 	)
 }
