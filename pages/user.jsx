12da77af384473a44596de0e6e3ca291a2b3bee8
Wiktoria Janaszek <wiktoria.janaszek@gmail.com>
2022-04-18 20:56:37 +0200

    account's trees on sale

--- pages/user.jsx
+++ pages/user.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ProductTile from "../components/global/productTile";
 import Hero from "../components/home/hero";
 import UserCollection from "../components/home/userCollection";
@@ -6,21 +6,26 @@ import style from "../styles/market/market.module.scss";
 import Image from "next/image";
 import Search from "../components/global/search";
 const UserPage = ({account, accountsTrees, putOnSale, endSale, treesOnSale, receiveFunds, contract, accountsFounds}) => {
-    const renderProductTileAccount = () => {
-        const productTiles = []
-        //console.log("tile",accountsTrees)
-        for (let i = 0; i<accountsTrees.length; i++) {
-            console.log(accountsTrees[i], "tile saleid ")
-            productTiles.push(<ProductTile id={accountsTrees[i].id} saleId={accountsTrees[i].saleId} genes={accountsTrees[i].tree.genes} putOnSale={putOnSale} saleId={accountsTrees[i].saleId} endSale={endSale}/>)
-        }
-        return (
-            <section className={"productsContainer"}>
-                {productTiles}
-            </section>
-        )
-    }
+    // const renderProductTileAccount = () => {
+    //     const productTiles = []
+    //     //console.log("tile",accountsTrees)
+    //     for (let i = 0; i<accountsTrees.length; i++) {
+    //         console.log(accountsTrees[i], "tile saleid ")
+    //         productTiles.push(<ProductTile id={accountsTrees[i].id} saleId={accountsTrees[i].saleId} genes={accountsTrees[i].tree.genes} putOnSale={putOnSale} saleId={accountsTrees[i].saleId} endSale={endSale}/>)
+    //     }
+    //     return (
+    //         <section className={"productsContainer"}>
+    //             {productTiles}
+    //         </section>
+    //     )
+    // }
+    const [saleTrees, setSaleTrees] = useState([])
+    useEffect(()=>{
+        renderProductsOnSale()
+    }, [ accountsTrees])
 
     const renderProductsOnSale = () => {
+        console.log(saleTrees)
         const productTiles = []
         for (let i = 0; i<accountsTrees.length; i++) {
             if(accountsTrees[i].saleId !== undefined){
@@ -30,12 +35,14 @@ const UserPage = ({account, accountsTrees, putOnSale, endSale, treesOnSale, rece
             }
 
         }
-        return (
-            <section className={style.productsContainer}>
-                {productTiles}
-            </section>
-        )
+        setSaleTrees([...productTiles])
+        // return (
+        //     <section className={style.productsContainer}>
+        //         {productTiles}
+        //     </section>
+        // )
     }
+
 
     return (
         <>
@@ -53,21 +60,25 @@ const UserPage = ({account, accountsTrees, putOnSale, endSale, treesOnSale, rece
                         {/*    {(accountsFounds!=0 && accountsFounds!=="0" && accountsFounds!=undefined)?<button onClick={async()=> await receiveFunds()}>receive founds</button>:null}*/}
                         {/*</div>*/}
                         {/*<div>your trees {(accountsTrees.length)?renderProductTileAccount() : null}</div>*/}
-                        <main className={style.container}>
-                            <div className={style.guideSection}>
-                                <div className={style.titleImageContainer}>
-                                    <Image src="/Rectangle14.svg" height={55} width={421}/>
-                                </div>
-                                <div className={style.tileTextContainer}>
-                                    <h3 className={style.titleText}>YOUR TREES ON SALE</h3>
-                                </div>
-                                <Search></Search>
-                            </div>
-                            <div className={style.productsContainer}>
-                                {(treesOnSale.length)?renderProductsOnSale() :null}
+                        {
+                            (saleTrees.length!==0)?
+                                <div className={style.container}>
+                                    <div className={style.guideSection}>
+                                        <div className={style.titleImageContainer}>
+                                            <Image src="/Rectangle14.svg" height={55} width={421}/>
+                                        </div>
+                                        <div className={style.tileTextContainer}>
+                                            <h3 className={style.titleText}>YOUR TREES ON SALE</h3>
+                                        </div>
+                                        <Search></Search>
+                                    </div>
+                                    <div className={style.productsContainer}>
+                                        {(treesOnSale.length)? saleTrees.map((tree)=> tree) :null}
 
-                            </div>
-                        </main>
+                                    </div>
+                                </div>: null
+                        }
+
 
                     </div>  : null
             }
