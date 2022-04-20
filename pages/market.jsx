042b267812191529a05ddb91bd69e2a8ac0ccd13
Wiktoria Janaszek <wiktoria.janaszek@gmail.com>
2022-04-20 19:28:09 +0200

    tree market

--- pages/market.jsx
+++ pages/market.jsx
@@ -15,7 +15,9 @@ const Market = ({ buyTreeFromSale}) => {
         for (let i = 0; i<treesOnSale.length; i++) {
             console.log(treesOnSale, treesOnSale.length)
             let treeId = treesOnSale[i].tree.TreeId
-            productTiles.push(<ProductTile market={true} tree={treesOnSale[i]} id={treesOnSale[i].tree.TreeId} saleId={treesOnSale[i].id} contract={contract} account={account} buyTreeFromSale={buyTreeFromSale} />)
+            let tree = trees.find((tree)=> tree.id == treeId)
+            console.log(tree)
+            productTiles.push(<ProductTile market={true} tree={tree} id={treesOnSale[i].tree.TreeId} saleId={treesOnSale[i].id} contract={contract} account={account} buyTreeFromSale={buyTreeFromSale} />)
 
             // productTiles.push(<ProductTile market={true} genes={trees[treeId].tree.genes} birthdate={trees[treeId].tree.birthdate} id={treesOnSale[i].tree.TreeId} saleId={treesOnSale[i].id} contract={contract} account={account} buyTreeFromSale={buyTreeFromSale} price={treesOnSale[i].tree.valueWei}/>)
         }
