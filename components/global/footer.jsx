c3d79f6da1043b2b69b178c899f20681b1b89e99
alekszych <aleks.zych@icloud.com>
2022-04-23 23:37:45 +0200

    fixed sapling section

---
+++ components/global/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import style from "../../styles/global/footer.module.scss"
+
+const Footer = () => {
+	return (
+		<footer className={style.footer}>
+			<h3>Foresta @</h3>
+			<p>All rights reserved.</p>
+		</footer>
+	)
+}
+
+export default Footer
