c84858463d6237e82bea5f6606d9f134f5ebec73
alekszych <aleks.zych@icloud.com>
2022-04-24 22:01:34 +0200

    tree page edited

--- pages/tree.jsx
+++ pages/tree.jsx
@@ -1,9 +1,8 @@
 import React, {useContext} from "react"
-import {useRouter} from "next/router"
 import NTree from "../nTree/NTree"
 import {BrowserContext} from "./_app"
 import style from "../styles/tree/tree.module.scss"
-import {BsArrowsMove} from "react-icons/bs";
+import {BsArrowsMove} from "react-icons/bs"
 
 const Tree = () => {
 	const {vw, vh} = useContext(BrowserContext)
@@ -15,7 +14,7 @@ const Tree = () => {
 				width={100 * vw}
 				height={100 * vh}
 				islandSize={5}
-				cameraPosition={{x: 10, y: 0, z: 10}}
+				cameraPosition={{x: 10, y: -2, z: 10}}
 				y={-3}
 			/>
 			<button className={style.moveButton}>
