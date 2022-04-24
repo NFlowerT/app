3d4cad4f363d89ba803f20ea989546e71d52d68a
alekszych <aleks.zych@icloud.com>
2022-04-24 23:02:30 +0200

    tree added

--- components/home/buySapling.jsx
+++ components/home/buySapling.jsx
@@ -1,14 +1,24 @@
-import React from "react"
+import React, {useContext} from "react"
 import style from "../../styles/home/buySapling.module.scss"
 import {RiCloseFill} from "react-icons/ri"
+import NTree from "../../nTree/NTree"
+import {BrowserContext} from "../../pages/_app"
 
 const BuySapling = ({setSapling, mint}) => {
+	const {rem, width, vw, vh} = useContext(BrowserContext)
 	return (
 		<div className={style.background}>
 			<div className={style.container}>
 				<RiCloseFill className={style.close} onClick={() => setSapling(false)}/>
-
-				<div className={style.treeContainer}></div>
+				<NTree
+					disabled={true}
+					width={width > 900 ? 20 * vw : 80 * vw}
+					height={width > 900 ? 30 * rem : 30 * vh}
+					cameraPosition={{x: 6, y: -2, z: 6}}
+					className={style.treeContainer}
+					y={-2.5}
+					age={12}
+				/>
 				<div className={style.textContainer}>
 					<h1>
 						BUY YOUR
